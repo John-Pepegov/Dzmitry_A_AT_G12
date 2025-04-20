4c4f5eee8fbf0a78ed83a1d92c0010693ae606ed
Dzmitry Averkin <dzmitry.averkin@gmail.com>
2025-04-20 22:01:45 +0200

    playground task 1

--- src/homework/read_write/ReadWrite7.java
+++ src/homework/read_write/ReadWrite7.java
@@ -1,5 +1,5 @@
 package homework.read_write;
 
 public class ReadWrite7 {
-    // создает какой-нибудь объект класса (добавить имплементацию интерфейса Serializable) из пакета playground.essence и записывает его в файл
+    // создает какой-нибудь объект класса (добавить имплементацию интерфейса Serializable) из пакета playground.homework.playground.homework.playground.essence и записывает его в файл
 }
