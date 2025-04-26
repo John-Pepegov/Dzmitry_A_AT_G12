da827b76c1695aa95ffe41a375bf6f7e7e183bd7
Dzmitry Averkin <dzmitry.averkin@gmail.com>
2025-04-26 13:06:18 +0200

    classwork + homework-playground-task6

---
+++ src/classwork/cw_26_04_25/myCollections.java
@@ -0,0 +1,30 @@
+package classwork.cw_26_04_25;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class myCollections {
+    public static void main(String[] args) {
+        List<String> myList = new ArrayList<>();
+
+        String text = "мама мыла раму";
+        String[] words = text.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+            myList.add(words[i]);
+        }
+
+        System.out.println(myList);
+        System.out.println("------------------");
+
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.println(myList.get(i));
+        }
+
+        System.out.println("------------------");
+
+        for (String s : myList) {
+            System.out.println(s);
+        }
+    }
+}
