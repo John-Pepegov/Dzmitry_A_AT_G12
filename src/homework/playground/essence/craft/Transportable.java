c7b598eac31b0b68ead20a88c243b076440ea9fb
Dzmitry Averkin <dzmitry.averkin@gmail.com>
2025-04-26 10:29:59 +0200

    playground tasks 1-5

--- src/homework/playground/essence/craft/Transportable.java
+++ src/homework/playground/essence/craft/Transportable.java
@@ -1,4 +1,16 @@
 package homework.playground.essence.craft;
 
+import homework.playground.essence.Matter;
+
 public interface Transportable {
+    default int move(int pointA, int pointB) {
+        System.out.println(String.format(
+                "I am %s, my name is %s and I am moving from point %d to point %d",
+                this.getClass().getSimpleName(),
+                ((Matter) this).getName(),
+                pointA,
+                pointB
+        ));
+        return pointB - pointA;
+    }
 }
