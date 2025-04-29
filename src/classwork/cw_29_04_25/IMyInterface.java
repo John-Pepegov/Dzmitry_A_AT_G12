2037a97bfb68666a4169e6269d5b37eb4207cada
Dzmitry Averkin <dzmitry.averkin@gmail.com>
2025-04-29 19:33:52 +0200

    classwork 29.04.2025

---
+++ src/classwork/cw_29_04_25/IMyInterface.java
@@ -0,0 +1,5 @@
+package classwork.cw_29_04_25;
+
+public interface IMyInterface {
+    double doSomething();
+}
--- src/homework/playground/essence/creatures/Crawlable.java
+++ src/homework/playground/essence/creatures/Crawlable.java
@@ -1,4 +1,21 @@
 package homework.playground.essence.creatures;
 
+import homework.playground.essence.Matter;
+
 public interface Crawlable {
+    default void crawl(String direction, int distance) {
+        System.out.println(String.format(
+                "I am %s, my name is %s and I am crawling to %s for %d units",
+                this.getClass().getSimpleName(),
+                ((Matter) this).getName(),
+                direction,
+                distance
+        ));
+
+        if (this instanceof Crocodile) {
+            System.out.println("wr-wr-wrr-r..");
+        } else if (this instanceof Beetle) {
+            System.out.println("vz-vz-vzz-zz..");
+        }
+    }
 }
