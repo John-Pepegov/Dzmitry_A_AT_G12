e7dba7a85fc01fd61c2863c96429d9ad666198d5
Dzmitry Averkin <dzmitry.averkin@gmail.com>
2025-05-11 17:44:11 +0200

    hw_stream - task 10 upd

--- src/homework/streams/DoublesRunner.java
+++ src/homework/streams/DoublesRunner.java
@@ -12,21 +12,18 @@ public class DoublesRunner {
         // Создать поток данных дробных чисел doubles из 8 чисел (33.42, 34.3, 0.79, 2.3426, 6.8, 13.24, 5.5, 769.9) через (Stream.of())
         int totalVolume = Stream.of(33.42, 34.3, 0.79, 2.3426, 6.8, 13.24, 5.5, 769.9)
                 // Округлить каждое число до целого
-                .mapToInt(x -> (int) Math.round(x))
-                .boxed()
+                .map(x -> (int) Math.round(x))
                 // На основании этого потока сгенерировать новый поток данных целых чисел из случайных чисел
                 // с границами от 0 до числа из исходного потока данных
-                .flatMap(x -> random.ints(0, x + 1)
-                        .limit(x)                  // можно выбрать фиксированное количество, или n
-                        .boxed())
+                .map(x -> random.nextInt(x + 1))
                 // Убрать дубликаты значений
                 .distinct()
                 // Заменить каждое число в получившемся потоке данных новым потоком
                 // состоящим из объектов Bubble в количестве равному заменяемому числу,
                 // создавая объекты с объемом равным числу из исходного потока и именем
                 // по маске "Bubble vol-" + i, где i - число из исходного потока
-                .flatMap(num -> IntStream.range(0, num)
-                        .mapToObj(i -> new Bubble(num, "Bubble vol-" + num)))
+                .flatMap(x -> IntStream.range(0, x)
+                        .mapToObj(i -> new Bubble(x, "Bubble vol-" + x)))
                 // Отпечатать в консоль каждый из объектов нового потока с новой строки
                 .peek(System.out::println)
                 .mapToInt(Bubble::getVolume)
