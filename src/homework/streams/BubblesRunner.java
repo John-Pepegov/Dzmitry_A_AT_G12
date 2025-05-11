5f32b273f307881021adef78cb76cbfcb6da6622
Dzmitry Averkin <dzmitry.averkin@gmail.com>
2025-05-11 18:00:05 +0200

    hw_stream - task 11 upd

--- src/homework/streams/BubblesRunner.java
+++ src/homework/streams/BubblesRunner.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class BubblesRunner {
     public static void main(String[] args) {
-        // Создать список пузырей (класса Bubble) bubbles из 3 пузырьков с обьемами (2, 4, 5)
+        // Создать список пузырей (класса Bubble) bubbles из 3 пузырьков с объемами (2, 4, 5)
         // и именами (CO2, O2, CO) соответственно, через (Arrays.asList())
         List<Bubble> bubbles = Arrays.asList(
                 new Bubble(2, "CO2"),
@@ -20,7 +20,7 @@ public class BubblesRunner {
                 // В потоке данных отфильтровать только те пузыри, объем которых больше 3
                 .filter(b -> b.getVolume() > 3)
                 // Отсортировать данные по имени в восходящем порядке
-                .sorted((b1, b2) -> b1.getName().compareTo(b2.getName()))
+                .sorted(Comparator.comparing(Bubble::getName))
                 // На основании существующего потока данных создать новый,
                 // в котором каждый новый Bubble имеет объем в 3 раза больше исходного
                 .map(b -> new Bubble(b.getVolume() * 3, b.getName()))
