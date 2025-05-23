1ed176c3c16a5397786997e383f7a0ace27ef457
Dzmitry Averkin <dzmitry.averkin@gmail.com>
2025-05-23 22:11:54 +0200

    hw unit_tests for daysofweek done

--- src/main/java/homework/daysofweek/DayOfWeekRunner.java
+++ src/main/java/homework/daysofweek/DayOfWeekRunner.java
@@ -4,6 +4,6 @@ public class DayOfWeekRunner {
     public static void main(String[] args) {
         System.out.println(DayOfWeekUtility.getDayOfWeek(1));
         System.out.println(DayOfWeekUtility.getDayOfWeek(9));
-        System.out.println(DayOfWeekUtility.getDayOfWeekNextYear(2));
+        // System.out.println(DayOfWeekUtility.getDayOfWeekNextYear(2));
     }
 }
--- src/test/java/daysofweek/test/junit/DayOfWeekUtilityJUnitTest.java
+++
@@ -1,4 +0,0 @@
-package daysofweek.test.junit;
-
-public class DayOfWeekUtilityJUnitTest {
-}
