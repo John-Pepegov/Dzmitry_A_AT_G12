d5c7c1e16573f14e2714bc41e6633daf2455a9ef
Dzmitry Averkin <dzmitry.averkin@gmail.com>
2025-06-03 20:58:35 +0200

    classwork 03.06.2025

--- src/test/java/classwork/Classwork_27_05_2025.java
+++ src/test/java/classwork/Classwork_27_05_2025.java
@@ -5,12 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
 import java.util.Locale;
 
-public class cw_27_05_25 {
+public class Classwork_27_05_2025 {
     public static void main(String[] args) throws InterruptedException {
 
         ChromeOptions options = new ChromeOptions();
@@ -20,15 +21,14 @@ public class cw_27_05_25 {
         String tomorrowDate = tomorrow.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         String tomorrowDay = tomorrow.getDayOfWeek().getDisplayName(TextStyle.FULL, new Locale("ru"));
 
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
         driver.get("https://google.com");
         driver.findElement(By.xpath("//button/div[contains(text(),'Отклонить все')]")).click();
         driver.findElement(By.xpath("//textarea[@title='Поиск']")).sendKeys("погода минск");
-        Thread.sleep(500);
         driver.findElement(By.xpath("//*[@role='listbox']/li[1]")).click();
-        Thread.sleep(1000);
         String weatherData = driver
-                .findElement(By.xpath("//*[contains(@aria-label,'Celsius " + tomorrowDay + " 12:00')]"))
+                .findElement(By.xpath("//*[contains(@aria-label,'Celsius " + tomorrowDay + " 12:00')][1]"))
                 .getAttribute("aria-label");
         String temperature = weatherData.split("°")[0];
         System.out.println("Завтра " + tomorrowDate + " " + tomorrowDay + " - температура в 12:00 будет " + temperature + "°C");
